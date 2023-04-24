d4d908af9b59a5c5de33700ac34079837707b1d8
Nick Wilson <Nicholas.Wilson.23@cmc.edu>
2023-04-23 17:17:38 -0700

    first commit

--- sql.normalized_batch/03.sql
+++ sql.normalized_batch/03.sql
@@ -4,8 +4,13 @@
 SELECT
     lang,
     count(DISTINCT id_tweets) as count
-FROM tweet_tags
-JOIN tweets USING (id_tweets)
-WHERE tag='#coronavirus'
+FROM (
+    SELECT DISTINCT t2.id_tweets,lang
+    FROM tweet_tags t1
+    JOIN tweet_tags t2 ON t1.id_tweets = t2.id_tweets
+    JOIN tweets ON t2.id_tweets = tweets.id_tweets
+    WHERE t1.tag='#coronavirus'
+      AND t2.tag LIKE '#%'
+) t
 GROUP BY lang
 ORDER BY count DESC,lang;
