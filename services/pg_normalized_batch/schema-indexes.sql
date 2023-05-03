6608ba94676819f782784f4143cd7d099362ba4b
Nick Wilson <Nicholas.Wilson.23@cmc.edu>
2023-05-02 22:21:25 -0700

    results

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,9 +1,4 @@
 CREATE INDEX tag_id_tweets_index ON tweet_tags(tag, id_tweets);
-CREATE INDEX id_tweets_tag_index ON tweet_tags(id_tweets, tag); -- (probly Q3)
-
-
--- Q3 and Q4, Q5
+CREATE INDEX id_tweets_tag_index ON tweet_tags(id_tweets, tag); 
 CREATE INDEX id_tweets_lang_index ON tweets(id_tweets, lang);
-CREATE INDEX lang_index on tweets(lang);
--- (gin)
 CREATE INDEX gin_index ON tweets USING gin(to_tsvector('english',text)) WHERE lang='en';
