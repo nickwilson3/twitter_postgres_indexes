208d630dca8fbf86d8350537fa9d30afcf3c5ed1
Nick Wilson <Nicholas.Wilson.23@cmc.edu>
2023-05-02 19:27:19 -0700

    schema indexes

--- services/pg_denormalized/schema-indexes.sql
+++ services/pg_denormalized/schema-indexes.sql
@@ -2,3 +2,4 @@ create index tweetsjsonb_idx_hashtags on tweets_jsonb USING gin((data->'entities
 create index tweetsjsonb_idx_hashtags2 on tweets_jsonb USING gin((data->'extended_tweet'->'entities'->'hashtags'));
 create index tweetsjsonb_idx_gin on tweets_jsonb USING gin(to_tsvector('english',COALESCE(data->'extended_tweet'->>'full_text', data->>'text'))) WHERE (data->>'lang'='en');
 
+
